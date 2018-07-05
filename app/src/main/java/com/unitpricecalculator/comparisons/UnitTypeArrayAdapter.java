5c52d2b5b3169dc4ddcc4179b843435c825cfa70
Stevie Kideckel <stevekideckel@gmail.com>
2018-07-04 21:32:41 -0400

    Make unit type text and spinner have larger text

--- app/src/main/java/com/unitpricecalculator/comparisons/UnitTypeArrayAdapter.java
+++ app/src/main/java/com/unitpricecalculator/comparisons/UnitTypeArrayAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.widget.ArrayAdapter;
 
+import com.unitpricecalculator.R;
 import com.unitpricecalculator.unit.UnitType;
 import com.unitpricecalculator.unit.Units;
 
@@ -13,7 +14,7 @@ import java.util.List;
 final class UnitTypeArrayAdapter extends ArrayAdapter<String> {
 
     UnitTypeArrayAdapter(Context context) {
-        super(context, android.R.layout.simple_spinner_dropdown_item, getValues(context.getResources()));
+        super(context, R.layout.unit_type_spinner_dropdown_item, getValues(context.getResources()));
     }
 
     private static List<String> getValues(Resources resources) {
