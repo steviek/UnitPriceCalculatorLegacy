ae66916d08d989ed9f79988b548ed6a1e17e8270
Stevie Kideckel <stevekideckel@gmail.com>
2018-11-18 10:18:43 +0000

    Clean up MainActivity

--- app/src/main/java/com/unitpricecalculator/main/MainActivity.java
+++ app/src/main/java/com/unitpricecalculator/main/MainActivity.java
@@ -24,211 +24,212 @@ import java.io.IOException;
 import javax.inject.Inject;
 
 public final class MainActivity extends BaseActivity
-        implements MenuFragment.Callback, SavedFragment.Callback {
+    implements MenuFragment.Callback, SavedFragment.Callback {
 
-    @Inject ObjectMapper objectMapper;
+  @Inject
+  ObjectMapper objectMapper;
 
-    private ActionBarDrawerToggle mDrawerToggle;
-    private DrawerLayout mDrawerLayout;
+  private ActionBarDrawerToggle mDrawerToggle;
+  private DrawerLayout mDrawerLayout;
 
-    private ComparisonFragment mComparisonFragment;
-    private SettingsFragment mSettingsFragment;
-    private SavedComparison mMainState;
-    private SavedFragment mSavedFragment;
+  private ComparisonFragment mComparisonFragment;
+  private SettingsFragment mSettingsFragment;
+  private SavedComparison mMainState;
+  private SavedFragment mSavedFragment;
 
-    private State mState;
+  private State mState;
 
-    private enum State {
-        MAIN, SETTINGS, SAVED
+  private enum State {
+    MAIN, SETTINGS, SAVED
+  }
+
+  @Override
+  protected final void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    Preconditions.checkNotNull(getSupportActionBar());
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    getSupportActionBar().setHomeButtonEnabled(true);
+
+    mDrawerLayout = findViewById(R.id.drawer_layout);
+    mDrawerToggle = new ActionBarDrawerToggle(
+        this, mDrawerLayout, R.string.app_name, R.string.app_name);
+
+    mDrawerToggle.setDrawerIndicatorEnabled(true);
+    mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+    mComparisonFragment = new ComparisonFragment();
+    mSettingsFragment = new SettingsFragment();
+    mSavedFragment = new SavedFragment();
+
+    if (savedInstanceState != null) {
+      mState = State.valueOf(savedInstanceState.getString("state"));
+      try {
+        mComparisonFragment.restoreState(
+            objectMapper.readValue(
+                savedInstanceState.getString("mainFragment"),
+                SavedComparison.class));
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    } else {
+      mState = State.MAIN;
     }
 
-    @Override
-    protected final void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    getSupportFragmentManager()
+        .beginTransaction()
+        .replace(R.id.content_frame, getFragment(mState))
+        .replace(R.id.menu_frame, new MenuFragment())
+        .commit();
+  }
 
-        Preconditions.checkNotNull(getSupportActionBar());
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    outState.putString("state", mState.name());
+    if (mComparisonFragment != null) {
+      try {
+        outState.putString(
+            "mainFragment",
+            objectMapper.writeValueAsString(mComparisonFragment.saveState()));
+      } catch (JsonProcessingException e) {
+        throw Throwables.propagate(e);
+      }
+    }
+  }
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerToggle = new ActionBarDrawerToggle(
-                this, mDrawerLayout, R.string.app_name, R.string.app_name);
+  @Override
+  protected void onPostCreate(Bundle savedInstanceState) {
+    super.onPostCreate(savedInstanceState);
+    // Sync the toggle state after onRestoreInstanceState has occurred.
+    mDrawerToggle.syncState();
+  }
 
-        mDrawerToggle.setDrawerIndicatorEnabled(true);
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    mDrawerToggle.onConfigurationChanged(newConfig);
+  }
 
-        mComparisonFragment = new ComparisonFragment();
-        mSettingsFragment = new SettingsFragment();
-        mSavedFragment = new SavedFragment();
+  @Override
+  public boolean onPrepareOptionsMenu(Menu menu) {
+    menu.clear();
+    switch (mState) {
+      case MAIN:
+        setTitle(R.string.app_name);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+      case SETTINGS:
+        setTitle(R.string.settings);
+        return true;
+      case SAVED:
+        setTitle(R.string.saved_comparisons);
+        return true;
+    }
+    return super.onPrepareOptionsMenu(menu);
+  }
 
-        if (savedInstanceState != null) {
-            mState = State.valueOf(savedInstanceState.getString("state"));
-            try {
-                mComparisonFragment.restoreState(
-                        objectMapper.readValue(
-                                savedInstanceState.getString("mainFragment"),
-                                SavedComparison.class));
-            } catch (IOException e) {
-                throw Throwables.propagate(e);
-            }
-        } else {
-            mState = State.MAIN;
-        }
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Pass the event to ActionBarDrawerToggle, if it returns
+    // true, then it has handled the app icon touch event
+    if (mDrawerToggle.onOptionsItemSelected(item)) {
+      return true;
+    }
+    // Handle your other action bar items...
+    switch (item.getItemId()) {
+      case R.id.action_save:
+        mComparisonFragment.save();
+        break;
+      case R.id.action_clear:
+        mComparisonFragment.clear();
+        break;
 
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.content_frame, getFragment(mState))
-                .replace(R.id.menu_frame, new MenuFragment())
-                .commit();
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString("state", mState.name());
-        if (mComparisonFragment != null) {
-            try {
-                outState.putString(
-                        "mainFragment",
-                        objectMapper.writeValueAsString(mComparisonFragment.saveState()));
-            } catch (JsonProcessingException e) {
-                throw Throwables.propagate(e);
-            }
+    return super.onOptionsItemSelected(item);
+  }
+
+  @Override
+  public void onMenuEvent(MenuFragment.MenuEvent event) {
+    switch (event) {
+      case FEEDBACK:
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("message/rfc822");
+        intent.putExtra(Intent.EXTRA_EMAIL, "sixbynineapps@gmail.com");
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.feedback_subject));
+        try {
+          startActivity(Intent.createChooser(intent, getString(R.string.send_email)));
+        } catch (ActivityNotFoundException e) {
+          Toast.makeText(this, R.string.no_email_client, Toast.LENGTH_SHORT).show();
         }
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        mDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
-
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        menu.clear();
-        switch (mState) {
-            case MAIN:
-                setTitle(R.string.app_name);
-                getMenuInflater().inflate(R.menu.menu_main, menu);
-                return true;
-            case SETTINGS:
-                setTitle(R.string.settings);
-                return true;
-            case SAVED:
-                setTitle(R.string.saved_comparisons);
-                return true;
-        }
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Pass the event to ActionBarDrawerToggle, if it returns
-        // true, then it has handled the app icon touch event
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        // Handle your other action bar items...
-        switch (item.getItemId()) {
-            case R.id.action_save:
-                mComparisonFragment.save();
-                break;
-            case R.id.action_clear:
-                mComparisonFragment.clear();
-                break;
-
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onMenuEvent(MenuFragment.MenuEvent event) {
-        switch (event) {
-            case FEEDBACK:
-                Intent intent = new Intent(Intent.ACTION_SEND);
-                intent.setType("message/rfc822");
-                intent.putExtra(Intent.EXTRA_EMAIL, "sixbynineapps@gmail.com");
-                intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.feedback_subject));
-                try {
-                    startActivity(Intent.createChooser(intent, getString(R.string.send_email)));
-                } catch (ActivityNotFoundException e) {
-                    Toast.makeText(this, R.string.no_email_client, Toast.LENGTH_SHORT).show();
-                }
-                break;
-            case NEW:
-                changeState(State.MAIN);
-                break;
-            case RATE:
-                Intent i = new Intent(Intent.ACTION_VIEW,
-                        Uri.parse(
-                                "https://play.google.com/store/apps/details?id=" +
-                                        "com.unitpricecalculator"));
-                startActivity(i);
-                break;
-            case SETTINGS:
-                changeState(State.SETTINGS);
-                break;
-            case SAVED:
-                changeState(State.SAVED);
-                break;
-            case SHARE:
-                break;
-        }
-    }
-
-    private Fragment getFragment(State state) {
-        switch (state) {
-            case SETTINGS:
-                return mSettingsFragment;
-            case MAIN:
-                return mComparisonFragment;
-            case SAVED:
-                return mSavedFragment;
-        }
-        throw new IllegalArgumentException("Unexpected state: " + state);
-    }
-
-    private void changeState(State state) {
-        mDrawerLayout.closeDrawers();
-        if (state == mState) {
-            return;
-        }
-
-        if (mState == State.MAIN) {
-            mMainState = mComparisonFragment.saveState();
-        }
-
-        switch (state) {
-            case MAIN:
-                mComparisonFragment.restoreState(mMainState);
-                break;
-            case SETTINGS:
-                hideSoftKeyboard();
-                break;
-            case SAVED:
-                hideSoftKeyboard();
-                break;
-        }
-        mState = state;
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.content_frame, getFragment(mState))
-                .commit();
-        invalidateOptionsMenu();
-    }
-
-    @Override
-    public void onLoadSavedComparison(SavedComparison comparison) {
-        mMainState = comparison;
+        break;
+      case NEW:
         changeState(State.MAIN);
+        break;
+      case RATE:
+        Intent i = new Intent(Intent.ACTION_VIEW,
+            Uri.parse(
+                "https://play.google.com/store/apps/details?id=" +
+                    "com.unitpricecalculator"));
+        startActivity(i);
+        break;
+      case SETTINGS:
+        changeState(State.SETTINGS);
+        break;
+      case SAVED:
+        changeState(State.SAVED);
+        break;
+      case SHARE:
+        break;
     }
+  }
+
+  private Fragment getFragment(State state) {
+    switch (state) {
+      case SETTINGS:
+        return mSettingsFragment;
+      case MAIN:
+        return mComparisonFragment;
+      case SAVED:
+        return mSavedFragment;
+    }
+    throw new IllegalArgumentException("Unexpected state: " + state);
+  }
+
+  private void changeState(State state) {
+    mDrawerLayout.closeDrawers();
+    if (state == mState) {
+      return;
+    }
+
+    if (mState == State.MAIN) {
+      mMainState = mComparisonFragment.saveState();
+    }
+
+    switch (state) {
+      case MAIN:
+        mComparisonFragment.restoreState(mMainState);
+        break;
+      case SETTINGS:
+        hideSoftKeyboard();
+        break;
+      case SAVED:
+        hideSoftKeyboard();
+        break;
+    }
+    mState = state;
+    getSupportFragmentManager()
+        .beginTransaction()
+        .replace(R.id.content_frame, getFragment(mState))
+        .commit();
+    invalidateOptionsMenu();
+  }
+
+  @Override
+  public void onLoadSavedComparison(SavedComparison comparison) {
+    mMainState = comparison;
+    changeState(State.MAIN);
+  }
 }
